c2e1a717183b7c032818b94cebfd54ffe00c73f6
eXeCutie <danielgehlert2007@gmailcom>
2026-04-27 17:58:00 +0200

    major html reform and addition

--- pages/determiner/determiners.js
+++
@@ -1,8 +0,0 @@
-console.log('hello world')
-import * as oop from '/pages/dictionary/dictionary-oop.js'
-import { DIALECTS } from "https://draconicconlang.github.io/APIs/dialects/DialectLoader.js";
-globalThis.dictionaryReady = DIALECTS.load("dr_dr").then(DR => {
-  Object.assign(globalThis, DR);
-  // init code
-  oop.htmlEditing.tables.detTable(document.getElementsByClassName('determinersTableTbody'));
-});
